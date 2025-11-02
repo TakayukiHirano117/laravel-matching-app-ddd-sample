9bb8d3905c536972c3d61d600bed889b9bf368ed
TakayukiHirano117 <ikusa.hirano@icloud.com>
2025-11-02 16:45:55 +0900

    [Add]usecaseの洗い出し

---
+++ app/Domain/DomainService/LikeDomainServiceInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Domain\DomainService;
+
+use App\Domain\Models\vo\UuidVo;
+
+interface LikeDomainServiceInterface
+{
+  public function isLikeExists(UuidVo $likeId): bool;
+}
