2a7a6ec043e66cd3f4f0e8e0366dd3bafe31c2fe
TakayukiHirano117 <ikusa.hirano@icloud.com>
2025-11-03 19:33:13 +0900

    [Update]いいね作成後、必要があればマッチングを成立させる様に改修

--- app/UseCase/Like/CreateLikeUseCase.php
+++ app/UseCase/Like/CreateLikeUseCase.php
@@ -6,44 +6,61 @@ use App\Domain\Repository\LikeRepositoryInterface;
 use App\Domain\Models\vo\UuidVo;
 use App\Domain\Models\Like\Like;
 use App\Domain\Repository\UserRepositoryInterface;
-use Ramsey\Uuid\Uuid;
 use App\Domain\DomainService\LikeDomainServiceInterface;
 use Exception;
+use App\Domain\DomainService\MatchingDomainServiceInterface;
+use App\Domain\Models\Match\Matching;
+use App\Domain\Repository\MatchingRepositoryInterface;
+use App\Domain\Repository\TransactionRepositoryInterface;
 class CreateLikeUseCase
 {
   public function __construct(
     private readonly LikeRepositoryInterface $likeRepository,
     private readonly UserRepositoryInterface $userRepository,
-    private readonly LikeDomainServiceInterface $likeDomainService
+    private readonly LikeDomainServiceInterface $likeDomainService,
+    private readonly MatchingDomainServiceInterface $matchingDomainService,
+    private readonly MatchingRepositoryInterface $matchingRepository,
+    private readonly TransactionRepositoryInterface $transactionRepository
   ) {
   }
 
   public function execute(array $input): void
   {
-    $userId = UuidVo::NewUuidByVal($input['user_id']);
-    $targetUserId = UuidVo::NewUuidByVal($input['target_user_id']);
+    $this->transactionRepository->transaction(function () use ($input) {
+      $userId = UuidVo::NewUuidByVal($input['user_id']);
+      $targetUserId = UuidVo::NewUuidByVal($input['target_user_id']);
 
-    if ($userId->equals($targetUserId)) {
-      throw new Exception('User and target user cannot be the same');
-    }
+      if ($userId->equals($targetUserId)) {
+        throw new Exception('User and target user cannot be the same');
+      }
 
-    $user = $this->userRepository->findById($userId);
-    $targetUser = $this->userRepository->findById($targetUserId);
+      $user = $this->userRepository->findById($userId);
+      $targetUser = $this->userRepository->findById($targetUserId);
+      if ($user === null) {
+        throw new Exception('User not found');
+      }
 
-    if ($user === null) {
-      throw new Exception('User not found');
-    }
+      if ($targetUser === null) {
+        throw new Exception('Target user not found');
+      }
 
-    if ($targetUser === null) {
-      throw new Exception('Target user not found');
-    }
+      // 既にいいねが存在していたらエラーを返す
+      $isLikeFromMeExists = $this->likeDomainService->isLikeExists($userId, $targetUserId);
+      if ($isLikeFromMeExists) {
+        throw new Exception('Like from me already exists');
+      }
 
-    // 既にいいねが存在していたらエラーを返す
-    if ($this->likeDomainService->isLikeExists($userId, $targetUserId)) {
-      throw new Exception('Like already exists');
-    }
+      $likeFromMe = new Like(UuidVo::create(), $user->getUserId(), $targetUser->getUserId());
+      $this->likeRepository->create($likeFromMe);
 
-    $like = new Like(UuidVo::create(), $user->getUserId(), $targetUser->getUserId());
-    $this->likeRepository->create($like);
+      // おたがいにいいねしていたらマッチングを成立させる
+      $isLikeFromTargetUserExists = $this->likeDomainService->isLikeExists($targetUserId, $userId);
+      $isMatchingExists = $this->matchingDomainService->isMatchingExists($userId, $targetUserId);
+
+      if ($isLikeFromTargetUserExists && !$isMatchingExists) {
+        $matching = new Matching(UuidVo::create(), $user->getUserId(), $targetUser->getUserId());
+        $this->matchingRepository->create($matching);
+      }
+    });
   }
 }
